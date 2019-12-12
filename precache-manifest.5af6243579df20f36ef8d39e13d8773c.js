a838c66377e9471a3dd749b2a1f22bbe8e6b606b
Stanyke <chibuike.stanley50@gmail.com>
2019-12-12 21:35:16 +0100

    Updates

---
+++ precache-manifest.5af6243579df20f36ef8d39e13d8773c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf46788012b6991c759703862f2350b",
+    "url": "/Stanyke/todo/tree/develop/frontend/index.html"
+  },
+  {
+    "revision": "c7f55a795d23e7651032",
+    "url": "/Stanyke/todo/tree/develop/frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "03aa2b3a990ed179df35",
+    "url": "/Stanyke/todo/tree/develop/frontend/static/js/2.775dfb88.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Stanyke/todo/tree/develop/frontend/static/js/2.775dfb88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7f55a795d23e7651032",
+    "url": "/Stanyke/todo/tree/develop/frontend/static/js/main.f7e7274f.chunk.js"
+  },
+  {
+    "revision": "71689e9a63cb8237336d",
+    "url": "/Stanyke/todo/tree/develop/frontend/static/js/runtime-main.44d641e0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Stanyke/todo/tree/develop/frontend/precache-manifest.5af6243579df20f36ef8d39e13d8773c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Stanyke/todo/tree/develop/frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
